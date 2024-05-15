f8efe4e17467be6bfb2a80ddf6d16a888d3534ad
nadineRoldao <vitorianadineroldao@gmail.com>
2024-05-15 03:50:03 -0300

    criando estrutura do projeto

---
+++ src/app/contact-module/components/contact-item/contact-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'contact-item',
+    templateUrl: 'contact-item.component.html',
+    styleUrls: ['contact-item.component.css']
+})
+export class ContactItemComponent {
+    
+}
---
+++ src/app/contact-module/models/contact.model.ts
@@ -0,0 +1,6 @@
+export interface Contact {
+    id: string
+    nome: string
+    tipo: string
+    valor: string
+}
